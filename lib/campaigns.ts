869e33e76bba16f5407f035e6bf8897451fa23f2
Alex Larente <52228051+larente@users.noreply.github.com>
2025-09-04 13:38:15 -0700

    chore: clean prisma singleton and remove unused ts-expect-error

--- lib/campaigns.ts
+++ lib/campaigns.ts
@@ -5,12 +5,10 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  // @ts-ignore
   if (!global.prisma) {
-    // @ts-ignore
     global.prisma = new PrismaClient();
   }
-  // @ts-ignore
+  prisma = global.prisma;
   prisma = global.prisma;
 }
 
